56fc7e2ec770ec86eb72c0f8da56f9ee5c13dd24
ushoza <ushoza@hotmail.ru>
2020-01-28 10:24:33 +0500

    Доавлены entity Farmework object context и сделано заполненеие формы редкатирования заказчика через Entity Farmework

--- EditCustomer.aspx.cs
+++ EditCustomer.aspx.cs
@@ -8,21 +8,27 @@ using System.Web.UI.WebControls;
 public partial class EditCustomer : System.Web.UI.Page
 {
     private Guid id;
+    CustomerManagementEntities context;
     protected void Page_Init(object sender, EventArgs e)
     {
         id = Guid.Parse(Request["id"]);
+        context = new CustomerManagementEntities();
     }
     protected void Page_Load(object sender, EventArgs e)
     {
         if(!IsPostBack)
         {
+            Customer customer = getCustomerById();
+            tbFirstName.Text = customer.FirstName;
+            tbLastName.Text = customer.LastName;
+            tbAdress.Text = customer.Address;
 
         }
     }
 
-    private void getCustomerById()
+    private Customer getCustomerById()
     {
-
+        return context.Customers.Where(x => x.ID == id).FirstOrDefault<Customer>();
     }
 
 }
